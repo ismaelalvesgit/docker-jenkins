5b99b7ebae8f7b5332bbcd7434a90a9a58e6c84a
ismaelalvesgit <cearaismael1997@gmail.com>
2020-05-18 20:35:45 -0300

    atribuido sshpass na v 1.0.0

--- deploy.bat
+++ deploy.bat
@@ -1,13 +1,13 @@
-set versao=0.1.0
-set repositorio='./V.0.1.0'
+set versao=1.0.0
+set repositorio='./V.1.0.0'
 
-ECHO "### Iniciando o build da versão %versao% ..."
+ECHO "### Iniciando o build da versão %versao% ======================>"
 docker build -t ismaelalvesdoc/jenkins:%versao% %repositorio%
 
-ECHO "### Iniciando o build da última versão ..."
+ECHO "<====================== Iniciando o build da última versão ======================>"
 docker build -t ismaelalvesdoc/jenkins .
-echo "### Enviando imagem na versão %versao% para o repositorio ..."
+echo "<====================== Enviando imagem na versão %versao% para o repositorio ======================>"
 docker push ismaelalvesdoc/jenkins:%versao%
-echo "### Enviando imagem na última versão para o repositorio ..."
+echo "<====================== Enviando imagem na última versão para o repositorio ======================>"
 docker push ismaelalvesdoc/jenkins
-echo "### Build Finalizado ..."
+echo "<====================== Build Finalizado ======================>"
